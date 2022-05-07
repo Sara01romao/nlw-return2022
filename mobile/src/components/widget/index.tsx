baaf8441b7d2f10994b4c87312cfb8c45b5976de
Sara Romão <sara-romao@live.com>
2022-05-07 15:40:44 -0300

    button

--- mobile/src/components/widget/index.tsx
+++ mobile/src/components/widget/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import { View} from 'react-native';
-import {styles} from './styles';
+import React, {useRef} from 'react';
+import {TouchableOpacity} from 'react-native';
+import {ChatTeardropDots} from 'phosphor-react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
-export function Widget() {
+import {styles} from './styles';
+import {theme} from '../../theme'
+
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand();
+  }
+
   return (
-    <View style={styles.container}>
-      
-    </View>
+    <>
+      <TouchableOpacity 
+       style={styles.button}
+       onPress={handleOpen}
+      >
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+
+      </BottomSheet>
+
+    </>
   )
 }
+
+export default gestureHandlerRootHOC(Widget);
